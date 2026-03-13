6e3ce0d80732ed5f2b17a519ab4ea566462cc397
mohamedbenloughmari <mohabenloughmari@gmail.com>
2026-03-13 09:34:25 +0100

    fixed bspline_array backward

--- KAN.py
+++ KAN.py
@@ -3,25 +3,26 @@ import torch.nn as nn
 from Cox_deboor import cox_deboor
 
 class bspline(nn.Module):
-    def __init__(self,degree=3,c_dim=10):
+    def __init__(self,p=3,c_dim=10):
         super(bspline, self).__init__()
-        self.t_dim = c_dim+degree+1
+        self.t_dim = c_dim+p+1
         self.c_dim = c_dim
         self.t=nn.Parameter(torch.randn(self.t_dim))
         self.c=nn.Parameter(torch.randn(c_dim))
-        self.p=degree
+        self.p=p
     def forward(self, x):
         return cox_deboor(self.t,self.c,self.p,x)
 
 class bspline_array(nn.Module):
-    def __init__(self, t_dim=10, c_dim=10, n=5):
+    def __init__(self, p=3, c_dim=10, n=5):
         super(bspline_array, self).__init__()
-        self.t_dim = t_dim
+        self.t_dim = c_dim+p+1
         self.c_dim = c_dim
         self.n = n
-        self.bsplines = nn.ModuleList([bspline(t_dim, c_dim) for _ in range(n)])
+        self.p= p
+        self.bsplines = nn.ModuleList([bspline(p, c_dim) for _ in range(n)])
     def forward(self, x):
-        return torch.stack([bs(x[:, i]) for i, bs in enumerate(self.bsplines)], dim=1)
+        return torch.stack([bs(x[i]) for i, bs in enumerate(self.bsplines)], dim=0)
 
 class KAN(nn.Module):
     def __init__(self, t_dim=10, c_dim=10, n=5, m=7):
@@ -37,10 +38,10 @@ class KAN(nn.Module):
 
 
 if __name__ == "__main__":
-    bs=bspline()
-    x=torch.tensor([1.0], requires_grad=True)
-    output=bs(x)
-    print("bs(x):", output)
-    torch.autograd.set_detect_anomaly(True)
-    output.backward()
+    #test backward on bspline_arrat
+    bs_array = bspline_array()
+    x = torch.tensor([1.0,0.2,0.5,0.8,0.3], requires_grad=True)
+    output = bs_array(x)
+    print("bs_array(x):", output)
+    output.sum().backward()
     print("dx/dx:", x.grad)
